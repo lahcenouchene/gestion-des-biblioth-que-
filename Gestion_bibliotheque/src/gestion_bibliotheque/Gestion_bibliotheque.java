7834ccdd497522e3b80a738224c4e3729b69dcf3
Your Name <your.email@example.com>
2024-03-24 00:54:50 +0100

    projet gestion d'immobilier

---
+++ Gestion_bibliotheque/src/gestion_bibliotheque/Gestion_bibliotheque.java
@@ -0,0 +1,14 @@
+
+package gestion_bibliotheque;
+import java.sql.*;
+
+public class Gestion_bibliotheque {
+
+    public static void main(String[] args) {
+     Page1 pg1 = new Page1();
+     pg1.show();
+        
+    
+}
+}
+    
